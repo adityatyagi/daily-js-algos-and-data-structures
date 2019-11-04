48428b92ab66d50a693c3382043e4e5c280e3a21
Aditya Tyagi <aditya.tyagi@ebabu.co>
2019-11-04 10:54:14 +0530

    vowels counter done

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -6,6 +6,26 @@ e.g vowelsCounter('anehizxcv') // will return 3
 
 function vowelsCounter(text) {
     // Code goes here
+
+    // using iterative approach
+    // let counter = 0;
+    // let vowels = ["a", "e", "i", "o", "u"];
+    // for(let char of text.toLowerCase()){
+    //     if(vowels.includes(char)){
+    //         counter++;
+    //     }
+    // }
+    // return counter;
+
+
+
+    // using regex
+    let matchingInstances = text.match(/[aeiou]/gi);
+    if(matchingInstances){
+        return matchingInstances.length;
+    } else {
+        return 0;
+    }
 }
 
 
