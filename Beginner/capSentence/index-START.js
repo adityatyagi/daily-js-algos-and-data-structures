837e19fa076b44cc9b26136b30ac659f7680a2f6
Aditya Tyagi <tyagi.aditya844747@gmail.com>
2019-11-08 11:18:18 +0530

    capitalize sentence completed

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -8,7 +8,31 @@ return the equivalent of the sentence when capitalised. E.g
 
 
 function capSentence(text) {
-   // Code goes here
+  // Code goes here
+
+  // METHOD 1 - using ForEach
+  // let lowerCaseString = text.toLowerCase().split(' ');
+  // let capsArray = [];
+  // lowerCaseString.forEach(word => {
+  //   capsArray.push(word[0].toUpperCase() + word.slice(1));
+  // });
+  // return capsArray.join(' ');
+
+
+  // METHOD 2 - using map and slice
+  // let lowerCaseString = text.toLowerCase().split(' ');
+  // let capsArray = [];
+  // capsArray = lowerCaseString.map(word => {
+  //   return word[0].toUpperCase() + word.slice(1);
+  // });
+  // return capsArray.join(' ');
+
+  // METHOD 3 - using map and replace
+  let lowerCaseString = text.toLowerCase().split(' ');
+  let capsArray = lowerCaseString.map(word => {
+    return word.replace(word[0], word[0].toUpperCase());
+  });
+  return capsArray.join(' ');
 }
 
 
