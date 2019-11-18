6fc1ccee00e73cb70e0cf23ade6a7706e99c5a98
Aditya Tyagi <tyagi.aditya844747@gmail.com>
2019-11-18 10:41:14 +0530

    hamming distance completed

--- Beginner/hammingDistance/index-START.js
+++ Beginner/hammingDistance/index-START.js
@@ -8,8 +8,21 @@ E.g hammingDistance('rover', 'river') // should return 1
 
 function hammingDistance(stringA, stringB) {
     // Code goes here
+    let result = 0;
+    if (stringA.length == stringB.length) {
+        for (let i = 0; i < stringA.length; i++) {
+            console.log('A ' + 'i: ' + i + ' with value ' + stringA[i]);
+            console.log('B ' + 'i: ' + i + ' with value ' + stringB[i]);
+            if (stringA[i].toLowerCase() !== stringB[i].toLowerCase()) {
+                result++;
+            }
+        }
+        return result;
+    } else {
+        throw new Error('The two strings are not of equal length');
+    }
 }
 
-
+// console.log(hammingDistance('river', 'rover'));
 
 module.exports = hammingDistance
