05bdf480cd03c8e90b8d17d609ed9e41a83e0d53
adityatyagi <tyagi.aditya844747@gmail.com>
2019-11-23 05:56:00 +0530

    is anagram completed

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -9,7 +9,52 @@ don't. E.g
 
 function isAnagram(stringA, stringB) {
     // Code goes here
+
+    // METHOD 1
+    // return sanitizeString(stringA) === sanitizeString(stringB);
+
+    // -------------------------------------------------------------------
+
+    // METHOD 2
+    if (stringA.length === stringB.length) {
+        let charMapOfStringA = charMap(stringA);
+        let charMapOfStringB = charMap(stringB);
+
+        // compare the charMap values by traversing over the object keys - for...in
+        for (let key in charMapOfStringA) {
+            if (charMapOfStringA[key] !== charMapOfStringB[key]) {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
+
 }
+
+function sanitizeString(str) {
+    // ^ in Regex means Negated Set - Match any character that is not in the set
+    str = str.toLowerCase().replace(/[^a-z\d]/g, '');
+    return str.split('').sort().join('');
+}
+
+function charMap(str) {
+    let charMap = {};
+
+    // traversing through a string - for...of
+    for (let char of str) {
+        if (charMap.hasOwnProperty(char)) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    };
+
+    return charMap;
+}
+
+// console.log(isAnagram('Scotchy is Scotch!', 'Scotch is Scotchy!'));
 
 
 module.exports = isAnagram
