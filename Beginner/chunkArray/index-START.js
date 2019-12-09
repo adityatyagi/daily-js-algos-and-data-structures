a10b44acbb33b4c9f4e7ebf7328648299ab6d1bd
Aditya Tyagi <tyagi.aditya844747@gmail.com>
2019-12-09 11:16:30 +0530

    array chunking started

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -1,12 +1,22 @@
 /* 
  Given two or more arrays, write a function that combines
  their elements into one array without any repetition. 
- E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
+ E.g  chunkArray([1,2,3,3,3], 2) // should return [1,2,3,4,5]
 */
 
 function chunkArray(array, size) {
     // Code goes here
+    let result = []
+    for (value of array) {
+        let lastArray = result[result.length - 1]
+        if (!lastArray || lastArray.length == size) {
+            result.push([value])
+        } else {
+            lastArray.push(value)
+        }
+    }
+    return result;
 }
 
-
-module.exports = chunkArray
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 2));
+// module.exports = chunkArray
