b5a016c57473aa23e693100978a9743047400e15
adityatyagi <tyagi.aditya844747@gmail.com>
2024-03-12 16:18:38 +0530

    throttle

--- js/debounce.js
+++ js/debounce.js
@@ -1,6 +1,28 @@
+// In JavaScript, a debounce function makes sure that your code is only triggered once per user input. Search box suggestions, text-field auto-saves, and eliminating double-button clicks are all use cases for debounce.
 function initApp() {
-    const searchInput = document.getElementById('search');
+    const searchInput = document.getElementById('debounce');
     console.log('🚀 ~ initApp ~ searchInput:', searchInput);
+
+    const changeHandler = (e) => {
+        console.log(e.target.value);
+    };
+
+    const debounce = (cb, delay) => {
+        let timeout;
+        return (...args) => {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                cb(...args);
+            }, delay);
+        };
+    };
+
+    const debouncedChangeHandler = debounce(
+        (e) => changeHandler(e),
+        2000
+    );
+
+    searchInput.addEventListener('input', debouncedChangeHandler);
 }
 
 // run the js one the DOM is loaded completely
---
+++ js/throttle.js
@@ -0,0 +1,42 @@
+// throttle: send immediately and then send after every x seconds. use for sending the most recent/updated thing to servers. It basically continually sends new request based on whatever the last input was. Used in resize window, scroll, mousemovement
+// it does not wait for everything to be finished first, wait for x seconds and then send (debounce)
+// there is NO CANCELLING of the current delay/wait period like it is in debouncing
+function initApp() {
+    const searchInput = document.getElementById('throttle');
+    function handleInput(e) {
+        console.log(e.target.value);
+    }
+
+    function throttle(cb, delay = 1000) {
+        let shouldWait = false;
+        let waitingArgs;
+        const timeoutFunc = () => {
+            if (waitingArgs === null) {
+                shouldWait = false;
+            } else {
+                cb(...waitingArgs);
+                waitingArgs = null;
+                setTimeout(timeoutFunc, delay);
+            }
+        };
+        return (...args) => {
+            // if we are in the waiting period, then return
+            if (shouldWait) {
+                waitingArgs = args;
+                return;
+            }
+
+            // call immediately
+            cb(...args);
+            shouldWait = true;
+            // wait for the delay
+            setTimeout(timeoutFunc, delay);
+        };
+    }
+    const throttledInputHandler = throttle(
+        (e) => handleInput(e),
+        2000
+    );
+    searchInput.addEventListener('input', throttledInputHandler);
+}
+document.addEventListener('DOMContentLoaded', initApp);
