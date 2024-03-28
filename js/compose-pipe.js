ae668e18656422a6cb8bdf5917379bd00406a20b
adityatyagi <tyagi.aditya844747@gmail.com>
2024-03-28 23:54:01 +0530

    compose-pipe

---
+++ js/compose-pipe.js
@@ -0,0 +1,33 @@
+// compose and pipe
+/**
+ * compose: right to left
+ * pipe: left to right
+ */
+
+const addFive = (num) => {
+    return num + 5;
+};
+
+const subtractTwo = (num) => {
+    return num - 2;
+};
+
+const multiplyFour = (num) => {
+    return num * 4;
+};
+
+function compose(...fns) {
+    return function (init) {
+        // let result = init;
+        // for (let i = fns.length - 1; i >= 0; i--) {
+        //     console.log('🚀 ~ fns[i]:', fns[i]);
+        //     result = fns[i](result);
+        // }
+        // return result;
+        return fns.reduceRight((acc, curr) => {
+            return curr(acc);
+        }, init);
+    };
+}
+const evaluate = compose(addFive, subtractTwo, multiplyFour);
+console.log(evaluate(5));
