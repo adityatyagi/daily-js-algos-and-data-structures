4884fa8e78f38a8e234dfa5c7789a4f9178e6e61
adityatyagi <tyagi.aditya844747@gmail.com>
2024-06-26 15:09:28 +0530

    reactivity

--- js/design-patterns/proxy.js
+++ js/design-patterns/proxy.js
@@ -28,3 +28,48 @@ const proxiedPerson = new Proxy(person, {
 console.log(proxiedPerson.name);
 proxiedPerson.age = 17; // will not change
 console.log(proxiedPerson.age);
+
+const car = {
+    name: 'bmw',
+};
+
+Object.defineProperty(car, 'getName', {
+    get: function () {
+        return this.name;
+    },
+});
+
+Object.defineProperty(car, 'setName', {
+    set: function (value) {
+        this.name = value;
+    },
+});
+
+console.log(car.name);
+console.log(car.getName);
+car.setName = 'audi';
+console.log(car.name);
+console.log(car.getName);
+car.range = 9000; // we were not able to track the range
+console.log(car.range);
+
+// using proxies, we can track the basic activites. proxy will act as an interceptor
+const car2 = {
+    name: 'maruti',
+};
+
+const handler = {
+    // target: target object for which we need to create a proxy off and monitor
+    // prop: the property of the object in consideration
+    // value: value of the property
+    set(target, prop, value) {
+        console.log(
+            `${prop} is changed from ${target[prop]} to ${value}`
+        );
+        target[prop] = value;
+    },
+};
+
+const monitorCarObj = new Proxy(car2, handler);
+monitorCarObj.name = 'hundai'; // can track the changes
+monitorCarObj.blog = 'team-bhp'; // can track the addition of new property
